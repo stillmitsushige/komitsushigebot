b4ab289da054c29a2a285f7b99f4ed02991000af
茂林 恭平 <kyohei.shigebayashi@IT-04470-M.local>
2021-10-07 03:40:15 +0900

    お手本そのまま

--- launcher.py
+++ launcher.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 import config
+
 bot = commands.Bot(command_prefix="!")
 
 
@@ -11,20 +12,29 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
-        # Bot からのメッセージには反応しない
+        # Botからのメッセージには反応しない
         # この判定をしないと無限ループが起きる
         return
 
     if "Bot" in message.content:
-        await message.channel.send(" は~い、Bot で~す ")
+        await message.channel.send("は～い、Botで～す")
 
-
-    # ほげ
+    # 下記の処理を追加
     await bot.process_commands(message)
 
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send(f" こん。{ctx.author.name}！")
-    
+    # 待機するメッセージのチェック関数
+    def check_message_author(msg):
+        return msg.author is ctx.author
+    # あいさつする既存の処理
+    await ctx.send(f"こんにちは、{ctx.author.name}さん。")
+    await ctx.send("ご気分はいかがでしょうか？")
+    # チェック関数に合格するようなメッセージを待つ
+    msg = await bot.wait_for('message', check=check_message_author)
+    # 受け取ったメッセージの内容を使って返信
+    await ctx.send(f"「{msg.content}」という気分なんですね。")
+
+
 bot.run(config.TOKEN)
